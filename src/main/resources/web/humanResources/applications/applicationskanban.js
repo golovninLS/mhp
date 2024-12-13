e1291de579de2c186f4976b572a236457b0a2da6
Ksenia Budanowa <budanowa.ksenia@gmail.com>
2024-12-13 13:04:48 +0300

    (refs #29964) [HR] отображение нанятых сотрудников

--- src/main/resources/web/humanResources/applications/applicationskanban.js
+++ src/main/resources/web/humanResources/applications/applicationskanban.js
@@ -1,10 +1,77 @@
 function applicationsKanbanReorder(controller, elements) {
     for (let i = 0; i < elements.length; i++) {
-        if (elements[i].applications.order !== i) {
+    if (elements[i].applications && elements[i].applications.order !== undefined && elements[i].applications.order !== i) {
             controller.changeProperty("order", elements[i].applications, i);
         }
     }
 }
+
+function createStatusStat(header, text) {
+    let statusStat = document.createElement("div");
+    statusStat.classList.add("project-kanban-status-budget");
+    statusStat.innerHTML = text;
+    header.appendChild(statusStat);
+}
+
+function createApplicationsCard(applications) {
+    let applicationsCard = document.createElement("div");
+    applicationsCard.classList.add("applications-kanban-card", "card");
+
+    if (applications.idBackground) {
+        applicationsCard.classList.add("text-bg-" + applications.idBackground);
+    }
+
+    if (applications.subject) {
+        let applicationsHeader = document.createElement("h5");
+        applicationsHeader.classList.add("applications-kanban-card-header", "card-header");
+        applicationsHeader.innerHTML = applications.subject;
+        applicationsCard.appendChild(applicationsHeader);
+    }
+
+    let applicationsContent = document.createElement("ul");
+    applicationsContent.classList.add("applications-kanban-card-content", "list-group", "list-group-flush");
+    applicationsCard.appendChild(applicationsContent);
+
+    let applicationsBody = document.createElement("il");
+    applicationsBody.classList.add("applications-kanban-card-body", "list-group-item");
+    applicationsContent.appendChild(applicationsBody);
+
+    if (applications.firstName) {
+        let applicationsFirstName = document.createElement("h6");
+        applicationsFirstName.classList.add("applications-kanban-card-author", "card-title", "text-body-secondary");
+        applicationsFirstName.innerHTML = applications.firstName;
+        applicationsBody.appendChild(applicationsFirstName);
+    }
+
+    let applicationsContentCaption = document.createElement("li");
+    applicationsContentCaption.classList.add("applications-kanban-card-assigned-to", "list-group-item");
+    applicationsContentCaption.innerHTML =
+        (applications.source ? `<div class="text-secondary">Источник: </div><div>${applications.source}</div>` : "") +
+        (applications.companyName ? `<div class="text-secondary">Компания: </div><div>${applications.companyName}</div>` : "") +
+        (applications.textPosition ? `<div class="text-secondary">Специализация: </div><div>${applications.textPosition}</div>` : "") +
+        (applications.currentViza ? `<div class="text-secondary">Виза: </div><div>${applications.currentViza}</div>` : "") +
+        (applications.phone ? `<div class="text-secondary">Телефон: </div><div>${applications.phone}</div>` : "") +
+        (applications.channelComunication ? `<div class="text-secondary">Канал: </div><div>${applications.channelComunication}</div>` : "");
+    applicationsContent.appendChild(applicationsContentCaption);
+
+    if (applications.deadline) {
+        let applicationsDeadline = document.createElement("div");
+        applicationsDeadline.classList.add("applications-kanban-card-deadline", "card-footer");
+        applicationsDeadline.innerHTML = applications.deadline;
+        applicationsCard.appendChild(applicationsDeadline);
+    }
+
+    if (applications.captionContact) {
+        let applicationsContentContact = document.createElement("div");
+        applicationsContentContact.classList.add("applications-kanban-card-deadline", "card-footer");
+        applicationsContentContact.innerHTML = applications.captionContact;
+        applicationsCard.appendChild(applicationsContentContact);
+    }
+
+    applicationsCard.applications = applications;
+    return applicationsCard;
+}
+
 function applicationsKanban() {
     return {
         render: function (element, controller) {
@@ -38,10 +105,14 @@ function applicationsKanban() {
         },
         update: function (element, controller, list, options) {
             if (element.drake) element.drake.destroy();
-             element.drake = dragula({
-                 copy: false,
-                 revertOnSpill: true
-             });
+
+            element.drake = dragula({
+                copy: false,
+                revertOnSpill: true,
+                invalid: function (el, handle) {
+                    return el.closest('.companies') !== null;
+                }
+            });
 
             while (element.kanban.lastElementChild) {
                 element.kanban.removeChild(element.kanban.lastElementChild);
@@ -54,7 +125,7 @@ function applicationsKanban() {
 
             for (const status of options.statuses) {
                 let statusDiv = document.createElement("div")
-                statusDiv.classList.add("applications-kanban-status");
+                    statusDiv.classList.add("applications-kanban-status");
                 if (status !== options.statuses[0])
                     statusDiv.classList.add("border-start");
 
@@ -68,98 +139,65 @@ function applicationsKanban() {
                 statusName.innerHTML = status.name;
                 statusHeader.appendChild(statusName);
 
-//                let statusNew = document.createElement("button");
-//                statusNew.classList.add("applications-kanban-status-new");
-//                statusNew.classList.add("btn");
-//                statusNew.classList.add("btn-light");
-//                statusNew.innerHTML = "<i class=\"bi bi-plus\"></i>";
-//                statusHeader.appendChild(statusNew);
+                //                let statusNew = document.createElement("button");
+                //                statusNew.classList.add("applications-kanban-status-new");
+                //                statusNew.classList.add("btn");
+                //                statusNew.classList.add("btn-light");
+                //                statusNew.innerHTML = "<i class=\"bi bi-plus\"></i>";
+                //                statusHeader.appendChild(statusNew);
                 if (status.stat) {
-                    let statusStat = document.createElement("div");
-                    statusStat.classList.add("project-kanban-status-budget");
-                    statusStat.innerHTML = status.stat;
-                    statusHeader.appendChild(statusStat);
+                    createStatusStat(statusHeader, status.stat);
                 }
 
-//                statusNew.addEventListener("click", function() {
-//                    controller.changeProperty("createapplications", null, status.id);
-//                });
+                //                statusNew.addEventListener("click", function() {
+                //                    controller.changeProperty("createapplications", null, status.id);
+                //                });
 
                 let statusBody = document.createElement("div");
                 statusBody.classList.add("applications-kanban-status-body");
 
+                let mhpBody = document.createElement("div");
+                mhpBody.classList.add("companies");
+                element.mhpBody = mhpBody;
+                if (options.mhp.amount) createStatusStat(mhpBody, options.mhp.amount);
+
+                let sahBody = document.createElement("div");
+                sahBody.classList.add("companies");
+                element.sahBody = sahBody;
+                if (options.sah.amount) createStatusStat(sahBody, options.sah.amount);
+
+                if (status.nameStatus.trim() == "Нанят") {
+                    statusBody.appendChild(mhpBody);
+                    statusBody.appendChild(sahBody);
+                }
+
                 for (const applications of list)
                     if (applications.status === status.id.toString()) {
-                        let applicationsCard = document.createElement("div");
-                        applicationsCard.classList.add("applications-kanban-card");
-                        applicationsCard.classList.add("card");
-                        if (applications.idBackground)
-                            applicationsCard.classList.add("text-bg-" + applications.idBackground);
+                        if (status.nameStatus.trim() == "Нанят") {
+                            if (applications.companyEmployee !== undefined && applications.companyEmployee == 1) {
+                                let applicationsCard = createApplicationsCard(applications);
+                                mhpBody.appendChild(applicationsCard);
 
-                        applicationsCard.addEventListener("click", function() {
-                            controller.changeObject(applications, true, applicationsCard);
-                        });
+                                applicationsCard.addEventListener("click", function () {
+                                    controller.changeObject(applications, true, applicationsCard);
+                                });
+                            } else if (applications.companyEmployee !== undefined && applications.companyEmployee == 2) {
+                                let applicationsCard = createApplicationsCard(applications);
+                                sahBody.appendChild(applicationsCard);
 
-                        if (applications.subject) {
-                            let applicationsHeader = document.createElement("h5");
-                            applicationsHeader.classList.add("applications-kanban-card-header");
-                            applicationsHeader.classList.add("card-header");
-                            applicationsHeader.innerHTML = applications.subject;
-                            applicationsCard.appendChild(applicationsHeader);
+                                applicationsCard.addEventListener("click", function () {
+                                    controller.changeObject(applications, true, applicationsCard);
+                                });
+                            }
+                        } else {
+                            let applicationsCard = createApplicationsCard(applications);
+                            statusBody.appendChild(applicationsCard);
+
+                            applicationsCard.addEventListener("click", function () {
+                                controller.changeObject(applications, true, applicationsCard);
+                            });
                         }
-
-                        let applicationsContent = document.createElement("ul");
-                        applicationsContent.classList.add("applications-kanban-card-content");
-                        applicationsContent.classList.add("list-group");
-                        applicationsContent.classList.add("list-group-flush");
-                        applicationsCard.appendChild(applicationsContent);
-
-                        let applicationsBody = document.createElement("il");
-                        applicationsBody.classList.add("applications-kanban-card-body");
-                        applicationsBody.classList.add("list-group-item");
-                        applicationsContent.appendChild(applicationsBody);
-
-                        if (applications.firstName) {
-                            let applicationsFirstName = document.createElement("h6");
-                            applicationsFirstName.classList.add("applications-kanban-card-author");
-                            applicationsFirstName.classList.add("card-title");
-                            applicationsFirstName.classList.add("text-body-secondary");
-                            applicationsFirstName.innerHTML = applications.firstName;
-                            applicationsBody.appendChild(applicationsFirstName);
-                        }
-
-                        let applicationsContentCaption = document.createElement("li");
-                        applicationsContentCaption.classList.add("applications-kanban-card-assigned-to");
-                        applicationsContentCaption.classList.add("list-group-item");
-                        applicationsContentCaption.innerHTML = (applications.source ? ("<div class=\"text-secondary\">Источник: </div>" + "<div>" + applications.source + "</div>") : "") +
-                                                               (applications.companyName ? ("<div class=\"text-secondary\">Компания: </div>" + "<div>" + applications.companyName + "</div>") : "") +
-                                                               (applications.textPosition ? ("<div class=\"text-secondary\">Специализация: </div>" + "<div>" + applications.textPosition + "</div>") : "") +
-                                                               (applications.currentViza ? ("<div class=\"text-secondary\">Виза: </div>" + "<div>" + applications.currentViza + "</div>") : "") +
-                                                               (applications.phone ? ("<div class=\"text-secondary\">Телефон: </div>" + "<div>" + applications.phone + "</div>") : "") +
-                                                               (applications.channelComunication ? ("<div class=\"text-secondary\">Канал: </div>" + "<div>" + applications.channelComunication + "</div>") : "")
-                                                               ;
-                        applicationsContent.appendChild(applicationsContentCaption);
-
-                        if (applications.deadline) {
-                            let applicationsDeadline = document.createElement("div");
-                            applicationsDeadline.classList.add("applications-kanban-card-deadline");
-                            applicationsDeadline.classList.add("card-footer");
-                            applicationsDeadline.innerHTML = (applications.deadline);
-                            applicationsCard.appendChild(applicationsDeadline);
-                        }
-
-                        if (applications.captionContact) {
-                            let applicationsContentContact = document.createElement("div");
-                            applicationsContentContact.classList.add("applications-kanban-card-deadline");
-                            applicationsContentContact.classList.add("card-footer");
-                            applicationsContentContact.innerHTML = (applications.captionContact);
-                            applicationsCard.appendChild(applicationsContentContact);
-                        }
-
-                        applicationsCard.applications = applications;
-                        statusBody.appendChild(applicationsCard);
                     }
-
                 statusDiv.appendChild(statusBody);
                 statusBody.status = status;
                 element.statusBody = statusBody;
@@ -184,16 +222,16 @@ function applicationsKanban() {
                 if (target === element.acceptButton) {
                     controller.changeProperty("hire", el.applications);
                     source.appendChild(el);
-                } else
-                if (target === element.declineButton) {
-                    controller.changeProperty("refuse", el.applications);
-                    source.appendChild(el);
-                } else
-                if (el.applications.status !== target.status.id.toString()){
-                    controller.changeProperty("status", el.applications, target.status.id);
-                    applicationsKanbanReorder(controller, target.children);
-                }
-
+                } else if (target === element.declineButton) {
+                        controller.changeProperty("refuse", el.applications);
+                        source.appendChild(el);
+                    } else if (target && target.querySelector('.companies')) {
+                        element.drake.cancel(true);
+                        return;
+                    } else if (el.applications.status !== target.status.id.toString()) {
+                            controller.changeProperty("status", el.applications, target.status.id);
+                            applicationsKanbanReorder(controller, target.children);
+                    }
             });
         },
         clear: function (element) {
